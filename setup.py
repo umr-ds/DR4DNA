9a735e2a7799f96dedfb3093edeba74e037a9ee6
Michael Schwarz <schwarzk@mathematik.uni-marburg.de>
2023-08-22 11:04:05 +0200

    modified setup.py to install all dependencies without having to run pip install -r requirements.txt + update version

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 from setuptools import setup, find_packages
-
 from pathlib import Path
 from setuptools.command.install import install
 import subprocess
@@ -10,7 +9,7 @@ import subprocess
 class CustomInstall(install):
     def run(self):
         install.run(self)  # Run the default installation first
-        subprocess.check_call(['pip', 'install', './NOREC4DNA'])
+        subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
 
 
 thisDir = Path(__file__).parent
@@ -26,7 +25,7 @@ kaitaiSetuptoolsCfg = {
     "inputDir": formatsPath
 }
 
-setup(name='DR4DNA', version='1.0.0', use_scm_version=True, kaitai=kaitaiSetuptoolsCfg,
+setup(name='DR4DNA', version='1.0.0b', use_scm_version=True, kaitai=kaitaiSetuptoolsCfg,
       packages=find_packages(),
       setup_requires=[
           'pip',
